5e790ddbb066cdd41cef4d4e8d4807dba1571f0c
Alireza Rafiei <a.rafiei@DEV-12>
2019-08-21 17:35:49 +0430

    UnitOfWork Added

---
+++ Samim.PresentationLayer/SamimRouteManager.cs
@@ -0,0 +1,24 @@
+﻿namespace Samim.PresentationLayer
+{
+	public static class SamimRouteManager
+	{
+		public static class AdminRole
+		{
+			public static string Controller = "AdminRole";
+			public static string Index()
+			{
+				return string.Format("/{0}/Index", Controller);
+			}
+
+			public static string Create()
+			{
+				return string.Format("/{0}/Create", Controller);
+			}
+
+			public static string Edit()
+			{
+				return string.Format("/{0}/Edit", Controller);
+			}
+		}
+	}
+}
